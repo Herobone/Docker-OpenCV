dac2c9253f944bee4c1394ac6d12b0b3f7f15a18
Herobone <theherobone@gmail.com>
2021-03-17 15:56:18 +0000

    Change to safer run

---
+++ images/xsession/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+OPENCV_VERSION=$(curl -sX GET "https://api.github.com/repos/opencv/opencv/releases/latest" \
+    | awk '/tag_name/{print $4;exit}' FS='[""]'); \
+echo "Version: ${OPENCV_VERSION}"
+docker build -t herobone/opencv:xsession -t "herobone/opencv:xsession_${OPENCV_VERSION}" .
--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/bin/sh
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
 docker run -it \
-    --env DISPLAY --env QT_X11_NO_MITSHM=1 \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    --device=/dev/video0:/dev/video0 \
-    herobone/opencv:full
+        --volume=$XSOCK:$XSOCK:rw \
+        --volume=$XAUTH:$XAUTH:rw \
+        --env="XAUTHORITY=${XAUTH}" \
+        --env="DISPLAY" \
+        --user="opencv" \
+        --device /dev/video0:/dev/video0 \
+    herobone/opencv:xsession \
+    bash
