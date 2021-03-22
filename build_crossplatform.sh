3cd79b2889bdcb1eb1a7d4717ea662fdb6aec32e
Julius G <julius.gesang@gmail.com>
2021-03-22 11:10:05 +0100

    Update build_crossplatform.sh

--- build_crossplatform.sh
+++ build_crossplatform.sh
@@ -7,4 +7,12 @@ sudo docker buildx inspect --bootstrap
 OPENCV_VERSION=$(curl -sX GET "https://api.github.com/repos/opencv/opencv/releases/latest" \
     | awk '/tag_name/{print $4;exit}' FS='[""]'); \
 echo "Version: ${OPENCV_VERSION}"
-sudo docker buildx build --platform linux/arm64,linux/amd64 -t herobone/opencv:full -t herobone/opencv:latest -t "herobone/opencv:full_${OPENCV_VERSION}" --push images/full
+sudo docker buildx build \
+        --platform linux/arm64,linux/amd64 \
+        -t herobone/opencv:full \
+        -t herobone/opencv:latest \
+        -t "herobone/opencv:full_${OPENCV_VERSION}" \
+        --cache-from herobone/opencv:full_cache \
+        --cache-to herobone/opencv:full_cache \
+        --push \
+    images/full
