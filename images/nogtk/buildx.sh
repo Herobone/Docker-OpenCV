768cc07de418b8785f439e98129ea66b390820c7
Julius G <theherobone@gmail.com>
2021-05-13 09:00:47 +0200

    Remove tests from python and nogtk

---
+++ images/nogtk/buildx.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+OPENCV_VERSION=$(curl -sX GET "https://api.github.com/repos/opencv/opencv/releases/latest" \
+    | awk '/tag_name/{print $4;exit}' FS='[""]'); \
+echo "Version: ${OPENCV_VERSION}"
+docker buildx build --platform linux/amd64,linux/arm64 --push -t herobone/opencv:nogtk -t "herobone/opencv:nogtk-${OPENCV_VERSION}" .
